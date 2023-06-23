b0d0438ad00aa9c6d5e3ea824431be675fa8b64f
zbrachinara <zbrachinara@gmail.com>
2023-06-22 21:08:29 -0400

    do proper logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #[tokio::main]
 async fn main() {
+    simple_logger::init_with_level(log::Level::Debug).unwrap();
+
     let secrets_file = include_bytes!("../conf.properties");
     let secrets = java_properties::read(secrets_file.as_slice()).unwrap();
     println!("{secrets:?}");
